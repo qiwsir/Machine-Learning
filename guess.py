d93f631b1213caa6aec6e706dd5234270bd24c97
qiweiyang123 <szh_abc@163.com>
2020-10-23 21:18:50 +0800

    第一次作业

--- guess.py
+++ guess.py
@@ -2,13 +2,14 @@ import random
 
 
 def guess_number():
+    # 随机生成100以内的整数
     a = random.randint(0, 100)
-    print(a)
     count = 0
     print("最多输入十次")
     while count <= 10:
         num = input("输入一个整数：")
         count += 1
+        # 若猜对，结束循环，输出循环次数
         if int(num) == a:
             print("您猜对了！猜测的次数：" + str(count))
             break
@@ -16,6 +17,7 @@ def guess_number():
             print("The number is bigger!")
         else:
             print("The number is smaller!")
+        # 超过循环次数，结束循环
         if count == 10:
             print("您已超过输入次数！")
             break
